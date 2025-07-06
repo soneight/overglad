f13d9c363013ed2fbf7afe92f9300e4f61cba737
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-07-06 22:52:02 +0300

    3_3 progress...

--- include/son8/overglad/v3_3.hxx
+++ include/son8/overglad/v3_3.hxx
@@ -80,6 +80,65 @@ namespace son8::overglad {
     { glVertexAttribP3uiv( index, type, normalized, value ); }
     SON8_OVERGLAD_DEPR vertex_attrib_P_4( GLuint index, GLenum type, GLboolean normalized, GLuint const *value )
     { glVertexAttribP4uiv( index, type, normalized, value ); }
+
+    SON8_OVERGLAD_DEPR vertex_attrib_I_pointer( GLuint index, GLint size, GLenum type, GLsizei stride, GLvoid const *pointer )
+    { glVertexAttribIPointer( index, size, type, stride, pointer ); }
+    SON8_OVERGLAD_DEPR vertex_attrib_divisor( GLuint index, GLuint divisor )
+    { glVertexAttribDivisor( index, divisor ); }
+    SON8_OVERGLAD_DEPR primitive_restart_index( GLuint index )
+    { glPrimitiveRestartIndex( index ); }
+    SON8_OVERGLAD_DEPR draw_arrays_instanced( GLenum mode, GLint first,
+        GLsizei count, GLsizei primcount )
+    { glDrawArraysInstanced( mode, first, count, primcount ); }
+    SON8_OVERGLAD_DEPR draw_elements_instanced( GLenum mode,
+        GLsizei count, GLenum type, GLvoid const *indices, GLsizei primcount )
+    { glDrawElementsInstanced( mode, count, type, indices, primcount ); }
+    SON8_OVERGLAD_DEPR draw_elements_base_vertex( GLenum mode,
+        GLsizei count, GLenum type, GLvoid const *indices, GLint basevertex )
+    { glDrawElementsBaseVertex( mode, count, type, indices, basevertex ); }
+    SON8_OVERGLAD_DEPR draw_range_elements_base_vertex( GLenum mode, GLuint start, GLuint end,
+        GLsizei count, GLenum type, GLvoid const *indices, GLint basevertex )
+    { glDrawRangeElementsBaseVertex( mode, start, end, count, type, indices, basevertex ); }
+    SON8_OVERGLAD_DEPR draw_elements_instanced_base_vertex( GLenum mode,
+        GLsizei count, GLenum type, GLvoid const *indices, GLsizei primcount, GLint basevertex )
+    { glDrawElementsInstancedBaseVertex( mode, count, type, indices, primcount, basevertex ); }
+    SON8_OVERGLAD_DEPR multi_draw_elements_base_vertex( GLenum mode,
+        GLsizei const *count, GLenum type, GLvoid const *const *indices, GLsizei primcount, GLint const *basevertex )
+    { glMultiDrawElementsBaseVertex( mode, count, type, indices, primcount, basevertex ); }
+    SON8_OVERGLAD_DEPR bind_buffer_range( GLenum target, GLuint index, GLuint buffer, GLintptr offset, GLsizeiptr size )
+    { glBindBufferRange( target, index, buffer, offset, size ); }
+    SON8_OVERGLAD_DEPR bind_buffer_base( GLenum target, GLuint index, GLuint buffer )
+    { glBindBufferBase( target, index, buffer ); }
+    SON8_OVERGLAD_DEPR map_buffer_range( GLuint buffer, GLintptr offset, GLsizeiptr length, GLbitfield access )
+    { return glMapBufferRange( buffer, offset, length, access ); }
+    SON8_OVERGLAD_DEPR flush_mapped_buffer_range( GLuint buffer, GLintptr offset, GLsizeiptr length )
+    { glFlushMappedBufferRange( buffer, offset, length ); }
+    SON8_OVERGLAD_DEPR copy_buffer_sub_data( GLenum readtarget, GLenum writetarget,
+        GLintptr readoffset, GLintptr writeoffset, GLsizeiptr size )
+    { glCopyBufferSubData( readtarget, writetarget, readoffset, writeoffset, size ); }
+    SON8_OVERGLAD_DEPR gen_vertex_arrays( GLsizei n, GLuint *arrays )
+    { glGenVertexArrays( n, arrays ); }
+    SON8_OVERGLAD_DEPR delete_vertex_arrays( GLsizei n, GLuint const *arrays )
+    { glDeleteVertexArrays( n, arrays ); }
+    SON8_OVERGLAD_DEPR bind_vertex_array( GLuint array )
+    { glBindVertexArray( array ); }
+    SON8_OVERGLAD_DEPR get_uniform_block_index( GLuint program, GLchar const *uniformBlockName )
+    { return glGetUniformBlockIndex( program, uniformBlockName ); }
+    SON8_OVERGLAD_DEPR get_active_uniform_block_name( GLuint program,
+        GLuint uniformBlockIndex, GLsizei bufSize, GLsizei *length, GLchar *uniformBlockName )
+    { glGetActiveUniformBlockName( program, uniformBlockIndex, bufSize, length, uniformBlockName ); }
+    SON8_OVERGLAD_DEPR get_active_uniform_block( GLuint program,
+        GLuint uniformBlockIndex, GLenum pname, GLint *params )
+    { glGetActiveUniformBlockiv( program, uniformBlockIndex, pname, params ); }
+    SON8_OVERGLAD_DEPR get_uniform_indices( GLuint program,
+        GLsizei uniformCount, GLchar const *const *uniformNames, GLuint *uniformIndices )
+    { glGetUniformIndices( program, uniformCount, uniformNames, uniformIndices ); }
+    SON8_OVERGLAD_DEPR get_active_uniform_name( GLuint program,
+        GLuint uniformIndex, GLsizei bufSize, GLsizei *length, GLchar *uniformName )
+    { glGetActiveUniformName( program, uniformIndex, bufSize, length, uniformName ); }
+    SON8_OVERGLAD_DEPR get_active_uniforms( GLuint program,
+        GLsizei uniformCount, GLuint const *uniformIndices, GLenum pname, GLint *params )
+    { glGetActiveUniformsiv( program, uniformCount, uniformIndices, pname, params ); }
 #endif//SON8_OVERGLAD_INCLUDE_DEPRECATED
 } // namespace son8::overglad
 
